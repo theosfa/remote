8a101b1ed90320eeaf95645effd42aa602e8326c
theosfa <theos.fa@gmail.com>
2024-10-21 18:00:26 -0400

    Updated help

--- remote-simple.py
+++ remote-simple.py
@@ -103,11 +103,11 @@ def main(args: list[str]) -> None:
         description='BHP Net Tool',
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog=textwrap.dedent('''Example: 2
-            netcat.py -t 192.168.1.108 -p 5555 -l -c #command shell
-            netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
-            netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat/etc/passwd\" # execute command
-            echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
-            netcat.py -t 192.168.1.108 -p 5555 # connect to server
+            remote-simple -t 192.168.1.108 -p 5555 -l -c #command shell
+            remote-simple -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
+            remote-simple -t 192.168.1.108 -p 5555 -l -e=\"cat/etc/passwd\" # execute command
+            echo 'ABC' | ./remote-simple -t 192.168.1.108 -p 135 # echo text to server port 135
+            remote-simple -t 192.168.1.108 -p 5555 # connect to server
         '''))
     parser.add_argument('-c', 
                         '--command',
